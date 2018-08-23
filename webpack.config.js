1aa133e573b24eccf8a1fd2c512a4dfd7de05b02
luyang1026 <sqush@aliyun.com>
2018-08-24 02:20:19 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './_src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
    plugins: [
